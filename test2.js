d706ca356c3d0282708d7b4fd278ff2d0971148d
PotatoAim101 <juliette.rossie@gmail.com>
2018-07-08 19:16:56 +0100

    Addition de commentaires et changement de la taille du tableau

--- test2.js
+++ test2.js
@@ -9,17 +9,25 @@ context.fillRect(0, 0, canvas.width, canvas.height);
 //les obstacles en forme de tableau 
 // 1 --> non destructibles, 2 --> destructible
 const obstacleMatrix = [
-	[0, 0, 1, 0, 0],
-	[0, 2, 1, 1, 2],
-	[2, 0, 0, 1, 0],
-	[1, 0, 0, 2, 0],
-	[1, 1, 0, 1, 1],
+	[0, 0, 2, 0, 0, 0, 1, 0, 0, 0],
+	[0, 1, 1, 1, 2, 0, 1, 0, 0, 0],
+	[2, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+	[1, 0, 0, 2, 0, 0, 0, 0, 0, 0],
+	[1, 0, 0, 0, 2, 1, 1, 2, 2, 1],
+	[1, 1, 1, 2, 1, 0, 0, 1, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+	[2, 1, 0, 1, 1, 0, 0, 2, 0, 0],
+	[0, 1, 2, 0, 1, 0, 0, 1, 0, 0],
+	[0, 0, 0, 2, 1, 0, 0, 1, 0, 0],
 ];
 
 //pour pouvoir afficher à la bonne taille
+//il suffit de changer les cases du tableau et la taille change automatiquement
+//le canvas est carré donc il faut un tavleau carré aussi
 tailleObstacle = canvas.width/obstacleMatrix.length;
 context.scale(tailleObstacle, tailleObstacle)
 
+//dessine les obstacles
 function drawObstacles(matrix) {
 	matrix.forEach((row, y) => {
 		row.forEach((value, x) => {
@@ -34,11 +42,14 @@ function drawObstacles(matrix) {
 	});
 }
 
+//le joueur avec une position et des bombs (j'ai pas encore géré)
 var player = {
 	position: {x: 0.5, y: 0.5}, //position initiale
 	bomb: 0,
 }
 
+//Dessine un cercle pour le joueur
+//pour être au milieu de la case il faut 0.5
 function drawPlayer(x, y, color) {
 	context.beginPath();
 	context.arc(x, y, 0.3, 0, 2 * Math.PI, false);
@@ -51,11 +62,12 @@ function draw() {
 	context.fillStyle = "#693647";
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
-	//puis
+	//puis on dessine les obstacle et le joueur
 	drawObstacles(obstacleMatrix);
 	drawPlayer(player.position.x, player.position.y, 'pink');
 }
 
+//keys : up, down, right, left
 function movePlayer(player, matrix) {
 	/* up : 38
 	   down : 40
@@ -92,7 +104,7 @@ function movePlayer(player, matrix) {
 	});
 }
 
-// il faut pas mettre movePlayer dans update
+// /!\ il faut pas mettre movePlayer dans update
 movePlayer(player, obstacleMatrix);
 function update() {
 	draw();
